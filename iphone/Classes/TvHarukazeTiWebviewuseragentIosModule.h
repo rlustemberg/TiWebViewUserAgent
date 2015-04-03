1c93325f5de98b2e560458de5c2f2e4461a678fb
Richard Lustemberg <richard@lustembergconsultancy.com>
2015-04-03 18:43:15 +0200

    Added 64bit compatibility

--- iphone/Classes/TvHarukazeTiWebviewuseragentIosModule.h
+++ iphone/Classes/TvHarukazeTiWebviewuseragentIosModule.h
@@ -1,17 +1,13 @@
 /**
- * Copyright 2012 Tomoya Narita
- *  http://harukaze.tv/
- *  http://d.hatena.ne.jp/harukazepc/
- *  Twitter: @harukazepc
+ * TiWebView
  *
- * This module is licensed under the MIT License
- *
- * Appcelerator Titanium is Copyright (c) 2009-2010 by Appcelerator, Inc.
- * and licensed under the Apache Public License (version 2)
+ * Created by Your Name
+ * Copyright (c) 2015 Your Company. All rights reserved.
  */
+
 #import "TiModule.h"
 
-@interface TvHarukazeTiWebviewuseragentIosModule : TiModule 
+@interface TvHarukazeTiWebviewuseragentIosModule : TiModule
 {
 }
 
--- iphone/Classes/TvHarukazeTiWebviewuseragentIosModule.m
+++ iphone/Classes/TvHarukazeTiWebviewuseragentIosModule.m
@@ -22,32 +22,32 @@
 // this is generated for your module, please do not change it
 -(id)moduleGUID
 {
-	return @"5551d255-9388-48ef-ba71-88defb53bf43";
+    return @"5551d255-9388-48ef-ba71-88defb53bf43";
 }
 
 // this is generated for your module, please do not change it
 -(NSString*)moduleId
 {
-	return @"tv.harukaze.ti.webview.useragent.ios";
+    return @"tv.harukaze.ti.webview.useragent.ios";
 }
 
 #pragma mark Lifecycle
 
 -(void)startup
 {
-	// this method is called when the module is first loaded
-	// you *must* call the superclass
-	[super startup];
+    // this method is called when the module is first loaded
+    // you *must* call the superclass
+    [super startup];
 }
 
 -(void)shutdown:(id)sender
 {
-	// this method is called when the module is being unloaded
-	// typically this is during shutdown. make sure you don't do too
-	// much processing here or the app will be quit forceably
-	
-	// you *must* call the superclass
-	[super shutdown:sender];
+    // this method is called when the module is being unloaded
+    // typically this is during shutdown. make sure you don't do too
+    // much processing here or the app will be quit forceably
+    
+    // you *must* call the superclass
+    [super shutdown:sender];
 }
 
 -(void)setWebViewUserAgent:(id)value
@@ -57,42 +57,42 @@
     [dictionnary release];
 }
 
-#pragma mark Cleanup 
+#pragma mark Cleanup
 
 -(void)dealloc
 {
-	// release any resources that have been retained by the module
-	[super dealloc];
+    // release any resources that have been retained by the module
+    [super dealloc];
 }
 
 #pragma mark Internal Memory Management
 
 -(void)didReceiveMemoryWarning:(NSNotification*)notification
 {
-	// optionally release any resources that can be dynamically
-	// reloaded once memory is available - such as caches
-	[super didReceiveMemoryWarning:notification];
+    // optionally release any resources that can be dynamically
+    // reloaded once memory is available - such as caches
+    [super didReceiveMemoryWarning:notification];
 }
 
 #pragma mark Listener Notifications
 
 -(void)_listenerAdded:(NSString *)type count:(int)count
 {
-	if (count == 1 && [type isEqualToString:@"my_event"])
-	{
-		// the first (of potentially many) listener is being added 
-		// for event named 'my_event'
-	}
+    if (count == 1 && [type isEqualToString:@"my_event"])
+    {
+        // the first (of potentially many) listener is being added
+        // for event named 'my_event'
+    }
 }
 
 -(void)_listenerRemoved:(NSString *)type count:(int)count
 {
-	if (count == 0 && [type isEqualToString:@"my_event"])
-	{
-		// the last listener called for event named 'my_event' has
-		// been removed, we can optionally clean up any resources
-		// since no body is listening at this point for that event
-	}
+    if (count == 0 && [type isEqualToString:@"my_event"])
+    {
+        // the last listener called for event named 'my_event' has
+        // been removed, we can optionally clean up any resources
+        // since no body is listening at this point for that event
+    }
 }
 
 @end
